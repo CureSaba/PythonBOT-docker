33470387600cbf68078c50c8b17318f0a42f0bc1
Keita Furumatsu <a6360152@gmail.com>
2024-07-13 23:36:04 +0900

    ^a^

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 while True:
     print("< start >\n")
     for i in range(5):
-        print("{i}\n")
+        print(i)
+        print("\n")
         time.sleep(1)
     print("< end >\n")
